34cc7f6ce35068d016d50f33caedb63298edf58c
Shreyash Vishwasrao <shreyashv0929@gmail.com>
2025-02-22 16:35:37 +0530

    Update error-handler.

--- src/lib/error-handler.js
+++ src/lib/error-handler.js
@@ -1,17 +1,21 @@
 import mongoose from "mongoose";
 
 export default class ApplicationError extends Error {
-  constructor(error, statusCode = 500) {
+  constructor(
+    message = "Failed to process request.",
+    statusCode = 500,
+    error = null
+  ) {
     if (error instanceof mongoose.Error.ValidationError) {
       super(error.message);
-      this.statusCode = 500;
-    } else if (error.code === 11000 && error.name == "MongoServerError") {
+      this.statusCode = 400;
+    } else if (error?.code === 11000 && error?.name === "MongoServerError") {
       const field = Object.keys(error.keyValue)[0];
       const value = error.keyValue[field];
       super(`${field}: ${value} is already in use.`);
       this.statusCode = 400;
     } else {
-      super("Failed to process request.");
+      super(message);
       this.statusCode = statusCode;
     }
   }
